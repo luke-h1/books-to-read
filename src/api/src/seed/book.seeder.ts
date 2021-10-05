0283b8c4f4c2693d0d5e64307e342da97527a605
luke-h1 <luke.howsam@yahoo.com>
2021-10-05 15:26:02 +0100

    add user & books seed

--- src/api/src/seed/book.seeder.ts
+++ src/api/src/seed/book.seeder.ts
@@ -1,1 +1,23 @@
-export {};
+/* eslint-disable no-await-in-loop */
+import { NestFactory } from '@nestjs/core';
+import * as faker from 'faker';
+import * as bcrypt from 'bcryptjs';
+import { randomInt } from 'crypto';
+import { AppModule } from '../app.module';
+import { BookService } from '../book/book.service';
+
+(async () => {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const bookService = app.get(BookService);
+
+  for (let i = 0; i < 30; i += 1) {
+    await bookService.save({
+      title: faker.lorem.words(11),
+      description: faker.lorem.words(11),
+      link: faker.internet.url(),
+      price: randomInt(10, 1000),
+      userId: randomInt(1, 50),
+    });
+  }
+  process.exit();
+})();
--- src/api/src/seed/user.seeder.ts
+++ src/api/src/seed/user.seeder.ts
@@ -1,1 +1,26 @@
-export {};
+/* eslint-disable no-await-in-loop */
+import { NestFactory } from '@nestjs/core';
+import * as faker from 'faker';
+import * as bcrypt from 'bcryptjs';
+import { randomInt } from 'crypto';
+import { AppModule } from '../app.module';
+import { BookService } from '../book/book.service';
+import { UserService } from '../user/user.service';
+
+(async () => {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const userService = app.get(UserService);
+
+  const hashed = await bcrypt.hash('123', 10);
+
+  for (let i = 0; i < 30; i += 1) {
+    await userService.save({
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: hashed,
+      isAdmin: false,
+    });
+  }
+  process.exit();
+})();
